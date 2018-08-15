39abb38d995a88eafad73a87befe5e08d0f2d9cb
Simon Kiflay <szkiflay@gmail.com>
2018-08-15 19:12:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+// for loop
+function forLoop(array){
+for(var i=0; i<25; i++){
+  if(i===1){
+    array.push("I am 1 strange loop.");
+  } else {
+  array.push(`I am ${i} strange loops.`);
+}
+}
+return array;
+}
+
+
+// while loop
+
+function whileLoop(number){
+let countdown = 20;
+while (countdown < 0){
+  countdown++;
+  console.log(`${countdown} 'done'`);
+}
+}
+
+// do while loop
+var i = 0;
+var arrary =[];
+function incrementVariable(arrary){
+  console.log();
+}
+do {i = i+1;
+ array = array + 1;
+} while (i===array.length > 0 && incrementVariable===array);
